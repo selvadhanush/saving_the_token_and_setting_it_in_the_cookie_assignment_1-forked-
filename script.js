8ab5194237225bf2eb3355f3adf6fa66e691bc59
selvadhanush <selva.dhanush.s81@kalvium.community>
2025-04-22 11:44:14 +0530

    sfs

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+const jwt = require('jsonwebtoken');
+const secret="yfgh"
 const encrypt = (payload) => {
   // encrypt the payload and return token
+  const tocken= jwt.sign(payload, secret, { expiresIn: 60 * 60 });
+  return tocken
 }
 
 const decrypt = (token) => {
   // return decoded payload
+  var decoded = jwt.verify(token,secret);
+    return decoded
 }
 
 module.exports = {
